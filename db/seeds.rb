512b5b20c913b12cf7eccc93c04ce619ff982344
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-09-13 16:00:04 +0200

    product images

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,7 @@ bouquet = Category.new(
   order: 10,
   display: true
 )
+print "1 category / "
 
 bouquet_printemps = Product.new(
   name: "Bouquet Printemps",
@@ -152,6 +153,7 @@ box_paniers = Category.new(
   order: 20,
   display: true
 )
+print "1 category / "
 
 flower_box = Product.new(
   name: "Flower Box",
@@ -216,6 +218,144 @@ mega_flower_box_amour = Product.new(
 file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609862/Solstice/Mega_Flower_Box_Amour_r3n5df.jpg")
 mega_flower_box_amour.photos.attach(io: file, filename: "image.png", content_type: "image/png")
 mega_flower_box_amour.save!
+print "1 product / "
+
+fleurs_sechees = Category.new(
+  name: "Fleurs Séchées",
+  order: 30,
+  display: true
+)
+print "1 category / "
+
+bouquet_fleurs_sechees = Product.new(
+  name: "Bouquet de Fleurs Séchées",
+  description: "Quelle que soit l'occasion à célébrer, ce somptueux bouquet de fleurs séchées ravira vos proches ou votre intérieur.",
+  min_price: 35,
+  order: 10,
+  available: true,
+  category: fleurs_sechees
+)
+file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609859/Solstice/Bouquet_de_Fleurs_Se%CC%81che%CC%81es_a4724s.jpg")
+bouquet_fleurs_sechees.photos.attach(io: file, filename: "image.png", content_type: "image/png")
+bouquet_fleurs_sechees.save!
+print "1 product / "
+
+orchidees = Category.new(
+  name: "Orchidées",
+  order: 40,
+  display: true
+)
+print "1 category / "
+
+coupe_orchidees = Product.new(
+  name: "Bouquet de Fleurs Séchées",
+  description: "Ces orchidée Phalaenopsis sont de véritable accroche-regard dans votre maison. Vous pensez offrir une orchidée  sans parvenir à vous décider? Nous allons vous aider! L’orchidée est la meilleure idée de cadeau pour prouver sa tendresse et son admiration envers quelqu’un. Elle fait partie des fleurs que vous pouvez offrir aussi bien à votre partenaire qu’à un parent ou un ami, car les sentiments qu’elle évoque ne sont pas uniquement liés à l’amour de couple.",
+  min_price: 40,
+  order: 10,
+  available: true,
+  category: orchidees
+)
+file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609859/Solstice/Coupe_d_Orchide%CC%81es_hmcmv9.jpg")
+coupe_orchidees.photos.attach(io: file, filename: "image.png", content_type: "image/png")
+coupe_orchidees.save!
+print "1 product / "
+
+orchidee_blanche = Product.new(
+  name: "Orchidée Blanche",
+  description: "Ces orchidée Phalaenopsis sont de véritable accroche-regard dans votre maison. Vous pensez offrir une orchidée  sans parvenir à vous décider? Nous allons vous aider! L’orchidée est la meilleure idée de cadeau pour prouver sa tendresse et son admiration envers quelqu’un. Elle fait partie des fleurs que vous pouvez offrir aussi bien à votre partenaire qu’à un parent ou un ami, car les sentiments qu’elle évoque ne sont pas uniquement liés à l’amour de couple.",
+  min_price: 30,
+  order: 20,
+  available: true,
+  category: orchidees
+)
+file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609858/Solstice/Orchide%CC%81e_Blanche_rxmqr7.jpg")
+orchidee_blanche.photos.attach(io: file, filename: "image.png", content_type: "image/png")
+orchidee_blanche.save!
+print "1 product / "
+
+deuil = Category.new(
+  name: "Deuil",
+  order: 50,
+  display: true
+)
+print "1 category / "
+
+coussin_deuil = Product.new(
+  name: "Coussin Deuil",
+  description: "Coussin funéraire dédié à un être cher, un dernier hommage pour l'accompagner jusqu'au repos éternel ! Une composition élaborée avec des fleurs fraîches de saison .",
+  min_price: 90,
+  order: 10,
+  available: true,
+  category: deuil
+)
+file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609866/Solstice/Coussin_Deuil_xkq9un.jpg")
+coussin_deuil.photos.attach(io: file, filename: "image.png", content_type: "image/png")
+coussin_deuil.save!
+print "1 product / "
+
+coussin_conique_deuil = Product.new(
+  name: "Coussin Conique Deuil",
+  description: "Coussin conique de fleurs pour deuil composée de fleurs de saison (coloris au choix ) . Une croix de deuil est réalisée pour témoigner de sentiments les plus profonds et de regrets.",
+  min_price: 120,
+  order: 20,
+  available: true,
+  category: deuil
+)
+file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609861/Solstice/Coeur_Deuil_dwf5hu.jpg")
+coussin_conique_deuil.photos.attach(io: file, filename: "image.png", content_type: "image/png")
+coussin_conique_deuil.save!
+print "1 product / "
+
+raquette_deuil = Product.new(
+  name: "Raquette Deuil",
+  description: "Une belle et ample composition de deuil réalisée par Langage Des Fleurs pour rendre un dernier hommage à vos proches disparus",
+  min_price: 150,
+  order: 30,
+  available: true,
+  category: deuil
+)
+file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609858/Solstice/Raquette_Deuil_jhblzw.jpg")
+raquette_deuil.photos.attach(io: file, filename: "image.png", content_type: "image/png")
+raquette_deuil.save!
+print "1 product / "
+
+coeur_deuil = Product.new(
+  name: "Coeur Deuil",
+  description: "Le cœur porte une forte symbolique et il serait dommage d'y renoncer en période de deuil. Pour exprimer votre attachement au défunt en lui rendant un dernier hommage . ",
+  min_price: 80,
+  order: 40,
+  available: true,
+  category: deuil
+)
+file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609861/Solstice/Coeur_Deuil_dwf5hu.jpg")
+coeur_deuil.photos.attach(io: file, filename: "image.png", content_type: "image/png")
+coeur_deuil.save!
+print "1 product / "
+
+devant_tombe_deuil = Product.new(
+  name: "Devant de Tombe Deuil",
+  description: "Un devant de tombe est positionné devant le cercueil lors de la cérémonie puis il est déplacé au pied de la tombe au cimetière. Il s'agit d'une composition élégante de fleur piquées, construite en hauteur mais cependant bien stable, ce qui lui permet d'être disposée en extérieur après les funérailles afin de fleurir le cimetière.",
+  min_price: 130,
+  order: 50,
+  available: true,
+  category: deuil
+)
+file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609863/Solstice/Devant_de_Tombe_Deuil_mmijou.jpg")
+devant_tombe_deuil.photos.attach(io: file, filename: "image.png", content_type: "image/png")
+devant_tombe_deuil.save!
+print "1 product / "
+
+croix_deuil = Product.new(
+  name: "Croix Deuil",
+  description: "Croix de fleurs pour deuil composée de fleurs de saison (coloris au choix ) . Une croix de deuil est réalisée pour témoigner de sentiments les plus profonds et de regrets.",
+  min_price: 50,
+  order: 60,
+  available: true,
+  category: deuil
+)
+file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609859/Solstice/Croix_Deuil_qingux.jpg")
+croix_deuil.photos.attach(io: file, filename: "image.png", content_type: "image/png")
+croix_deuil.save!
 print "1 product / "
 
 
