17164681a007399bf013e1ea0488e11dc01b5da5
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-09-13 09:49:34 +0200

    products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,51 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :show]
+  skip_before_action :authenticate_user!, only: [:show]
+  before_action :set_product, only: %i[show edit update destroy]
+
+  def index
+    @products = Product.all
+  end
+
   def show
-    @product = Product.find(params[:id])
     @order_item = OrderItem.new
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: 'Product was successfully created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product), notice: 'Product was successfully updated'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, status: :see_other
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :min_price)
   end
 end
