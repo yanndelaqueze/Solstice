d3c9cb5ac5a3463951aa6985faa0574021452551
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-09-20 15:13:14 +0200

    pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[show edit update destroy]
 
+  def index
+    @categories = Category.all
+  end
+
   def show
     @products = @category.products
   end
