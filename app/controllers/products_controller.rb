7f339b0c938c80a3abff425a0b47d5dd23affd15
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-09-26 14:34:55 +0200

    wip

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,13 +11,17 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @category = Category.find(params[:category_id])
     @product = Product.new
   end
 
   def create
+    @category = Category.find(params[:category_id])
     @product = Product.new(product_params)
+    @product.category = @category
+    raise
     if @product.save
-      redirect_to products_path, notice: 'Product was successfully created'
+      redirect_to category_path(@product.category), notice: 'Product was successfully created'
     else
       render :new, status: :unprocessable_entity
     end
