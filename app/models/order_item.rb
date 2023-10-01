08f167e825cdae6de61b5958cbbc095611986fc6
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-10-01 21:46:17 +0200

    product show & form

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,8 @@
 class OrderItem < ApplicationRecord
+  OCCASIONS = ["Spontané", "Bonne fête !", "Remerciement", "Mariage", "Anniversaire", "Anniversaire de mariage", "Deuil", "Saint Valentin", "Commémoration", "Bon rétablissement", "Fête des mères", "Pour se faire plaisir", "Fête des grands-mères"]
   belongs_to :product
   belongs_to :order
   belongs_to :user, optional: true
+
+  validates :occasion, inclusion: { in: OCCASIONS }
 end
